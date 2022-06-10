5a5bd109c9a3dd8a0ee09a8c1b85b0892293fbb3
itgitman <9078836@qq.com>
2022-06-11 07:20:07 +0800

    added

---
+++ src/main/java/com/example/springbootdemo/model/User.java
@@ -0,0 +1,42 @@
+package com.example.springbootdemo.model;
+
+/**
+ * @Author lenovo
+ * @Description:
+ * @Date created at 7:18 2022/6/11
+ * @Modified by lenovo
+ **/
+public class User {
+    private Long id;
+    private String name;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
