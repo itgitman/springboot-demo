597b35d44afae72089150def22262e5d6cdef835
itgitman <9078836@qq.com>
2022-06-11 07:29:54 +0800

    Update UserController.java

--- src/main/java/com/example/springbootdemo/controller/UserController.java
+++ src/main/java/com/example/springbootdemo/controller/UserController.java
@@ -1,1 +1,10 @@
+package com.example.springbootdemo.controller;
 
+/**
+ * @Author lenovo
+ * @Description:
+ * @Date created at 7:29 2022/6/11
+ * @Modified by lenovo
+ **/
+public class UserController {
+}
