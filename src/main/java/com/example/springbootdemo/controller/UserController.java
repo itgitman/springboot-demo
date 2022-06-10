7d6628bf96c1a4c480deea2c1511c2d51381e700
itgitman <9078836@qq.com>
2022-06-11 07:31:32 +0800

    Update UserController.java

--- src/main/java/com/example/springbootdemo/controller/UserController.java
+++ src/main/java/com/example/springbootdemo/controller/UserController.java
@@ -1,10 +1,17 @@
 package com.example.springbootdemo.controller;
 
+import com.example.springbootdemo.model.User;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Author lenovo
  * @Description:
  * @Date created at 7:29 2022/6/11
  * @Modified by lenovo
  **/
+@RestController
 public class UserController {
+    public User findById(Long id) {
+        return new User(1L, "Alice", "123456");
+    }
 }
