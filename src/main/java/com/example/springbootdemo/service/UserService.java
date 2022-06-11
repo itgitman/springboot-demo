489a443173bcf2ced3939fee11d573942effc477
itgitman <9078836@qq.com>
2022-06-11 09:01:09 +0800

    Update UserService.java

--- src/main/java/com/example/springbootdemo/service/UserService.java
+++ src/main/java/com/example/springbootdemo/service/UserService.java
@@ -1,2 +1,11 @@
+package com.example.springbootdemo.service;
+
+import com.example.springbootdemo.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    public User findById(Long id) {
+        return new User(1L, "Alice", "123456");
+    }
 }
